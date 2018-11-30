525bfe8432f749afd9b860193c962bc376627d50
lgotarra <97gotarra@gmail.com>
2018-11-30 12:21:39 +0100

    Estat inicial. No hi ha res

---
+++ app/src/main/java/laura/gotarra/tabmyscores/TabActivity.java
@@ -0,0 +1,13 @@
+package laura.gotarra.tabmyscores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+    }
+}
