67442392710c01ee6305c96e74231f2423617df5
lgotarra <97gotarra@gmail.com>
2018-12-14 14:09:00 +0100

    Recycler View Horitzontal funcionant. Afegides noves classes pels items del Recycler View. Cal completar layout i continuar amb el ViewPager

--- app/src/main/java/laura/gotarra/tabmyscores/TabActivity.java
+++ app/src/main/java/laura/gotarra/tabmyscores/TabActivity.java
@@ -1,8 +1,13 @@
 package laura.gotarra.tabmyscores;
 
 import android.graphics.Canvas;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -11,24 +16,72 @@ import java.util.List;
 public class TabActivity extends AppCompatActivity {
 
     TabView test;
-    //Canvas canvas;
-    ArrayList<Integer> frets;
+    private ArrayList<TabItem> frets_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab);
 
-        frets = new ArrayList<Integer>();
+        ArrayList<Integer> frets = new ArrayList<Integer>();
         frets.add(3);
         frets.add(3);
-        frets.add(3);
-        frets.add(6);
-        frets.add(5);
         frets.add(4);
+        frets.add(5);
+        frets.add(5);
+        frets.add(3);
+        TabItem item1 = new TabItem("Sol",frets);
 
-        test = findViewById(R.id.tabView);
-        test.setChords_frets(frets);
+        ArrayList<Integer> frets_1 = new ArrayList<Integer>();
+        frets_1.add(1);
+        frets_1.add(1);
+        frets_1.add(2);
+        frets_1.add(3);
+        frets_1.add(3);
+        frets_1.add(1);
+        TabItem item2 = new TabItem("Fa", frets_1);
+
+        frets_list = new ArrayList<>();
+        frets_list.add(item1);
+        frets_list.add(item2);
+
+        RecyclerView TabList = findViewById(R.id.tabRecycler);
+        TabList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        TabList.setAdapter(new TabAdapter());
+
+
+    }
+
+    class TabViewHolder extends RecyclerView.ViewHolder {
+        private TabView tabView;
+        private TextView textView;
+
+        public TabViewHolder (View itemView) {
+            super(itemView);
+            this.tabView = itemView.findViewById(R.id.tabView);
+            this.textView = itemView.findViewById(R.id.textView);
+        }
+    }
+
+    class TabAdapter extends RecyclerView.Adapter<TabViewHolder> {
+
+        @NonNull
+        @Override
+        public TabViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View itemView = getLayoutInflater().inflate(R.layout.activity_item_tab, parent, false);
+            return new TabViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TabViewHolder holder, int position) {
+            TabItem item  = frets_list.get(position);
+            holder.tabView.setChords_frets(item.getChord_frets());
+            holder.textView.setText(item.getChord_name());
+        }
+
+        public int getItemCount() { return frets_list.size(); }
+
+
     }
 
 
--- app/src/main/java/laura/gotarra/tabmyscores/TabView.java
+++ app/src/main/java/laura/gotarra/tabmyscores/TabView.java
@@ -73,37 +73,42 @@ public class TabView extends View {
         circle.setStyle(Paint.Style.FILL);
         circle.setColor(Color.RED);
 
-        float y = 30;
+        final int width = canvas.getWidth() - ((int)Math.floor(lines.getStrokeWidth())) / 2;
+        final int height = canvas.getHeight();
+        final int vertsep = (height/10);
+
+        float y = vertsep;
         float x = 1;
-        float x_dist = (canvas.getWidth()-lines.getStrokeWidth()*10)/4;
+        float x_dist = (width - lines.getStrokeWidth()*10)/4;
+        float r = width/40;
 
         for (int i = 0; i < 6; i++) {
-            canvas.drawLine(0, y, canvas.getWidth(), y, lines);
+            canvas.drawLine(0, y, x_dist*4, y, lines);
             // canvas.drawRect(0, y, canvas.getWidth(), 10+y, lines);
-            y+=40;
+            y += vertsep;
         }
-        for (int i = 0; i<4; i++) {
-            canvas.drawLine(x,30,x,y-40,lines);
+        for (int i = 0; i<5; i++) {
+            canvas.drawLine(x,vertsep,x,y-vertsep,lines);
             //canvas.drawRect(x,30,x+10,y-40,lines);
-            x+= x_dist;
+            x += x_dist;
         }
 
 
         Rect bounds = new Rect();
         text.getTextBounds(Integer.toString(min), 0, 1, bounds);
-        x = canvas.getWidth()/8 - bounds.width()/2;
+        x = width/8 - bounds.width()/2 - r/4;
         for (int i = min; i < min + 5; i++) {
             text.getTextBounds(Integer.toString(i), 0, 1, bounds);
-            canvas.drawText(Integer.toString(i), x, y+3, text);
-            x += canvas.getWidth()/4 - bounds.width()/2;
+            canvas.drawText(Integer.toString(i), x, y+vertsep/4, text);
+            x += width/4 - bounds.width()/2;
         }
 
-        y = 30;
+        y = vertsep;
         for (int i = 0; i < 6; i++){
             if (chords_frets.get(i) > 0) {
-                canvas.drawCircle((chords_frets.get(i)-min)*x_dist +x_dist/2,y,15, circle);
+                canvas.drawCircle((chords_frets.get(i)-min)*x_dist +x_dist/2,y,r, circle);
             }
-            y+=40;
+            y+=vertsep;
         }
 
     }
